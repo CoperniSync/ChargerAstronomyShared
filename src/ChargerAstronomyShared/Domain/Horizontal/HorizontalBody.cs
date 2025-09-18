f5b4c651b1d30cd45e190fa203d445aa11b0ca0d
djoogway <theflaffywaffe@gmail.com>
2025-09-18 17:48:43 -0500

    horizontal object

---
+++ src/ChargerAstronomyShared/Domain/Horizontal/HorizontalBody.cs
@@ -0,0 +1,17 @@
+﻿
+using ChargerAstronomyShared.Domain.Equatorial;
+
+namespace ChargerAstronomyShared.Domain.Horizontal
+{
+    public abstract class HorizontalBody
+    {
+        /// <summary>
+        /// The angle in decimal degrees formed between the horizon and the star
+        /// </summary>
+        public double Altitude { get; internal set; }
+        /// <summary>
+        /// The angle in decimal degrees formed between due north and the star
+        /// </summary>
+        public double Azimuth { get; internal set; }
+    }
+}
---
+++ src/ChargerAstronomyShared/Domain/Horizontal/HorizontalStar.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargerAstronomyShared.Domain.Horizontal
+{
+    public sealed class HorizontalStar : HorizontalBody
+    {
+
+    }
+}
