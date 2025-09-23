d59428fdb1167a76f08076bc9c9a7c3fd059a50b
djoogway <theflaffywaffe@gmail.com>
2025-09-23 00:09:26 -0500

    horizontal sun

---
+++ src/ChargerAstronomyShared/Domain/Horizontal/HorizontalStar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ChargerAstronomyShared.Domain.Equatorial;
+
+namespace ChargerAstronomyShared.Domain.Horizontal
+{
+    /// <summary>
+    /// A star located by the Horizontal Coordinate method
+    /// </summary>
+    public class HorizontalStar : HorizontalBody
+    {
+        private EquatorialStar EquatorialStar;
+
+        /// <summary>
+        /// Creates a new object by wrapping an <see cref="EquatorialStar"/>
+        /// </summary>
+        /// <param name="body">The <see cref="EquatorialStar"/> to base this object off of.</param>
+        public HorizontalStar(EquatorialStar body) : base(body)
+        {
+            EquatorialStar = body;
+        }
+
+        /// <summary>
+        /// <see cref="EquatorialStar.StarId"/>
+        /// </summary>
+        public int StarId { get { return EquatorialStar.StarId; } }
+
+        /// <summary>
+        /// <see cref="EquatorialStar.HipparcosId"/>
+        /// </summary>
+        public int? HipparcosId { get { return EquatorialStar.HipparcosId; } }
+
+        /// <summary>
+        /// <see cref="EquatorialStar.ProperName"/>
+        /// </summary>
+        public string? StarName { get { return EquatorialStar.ProperName ?? $"HipId: {EquatorialStar.HipparcosId}"; } }
+
+        /// <summary>
+        /// <see cref="EquatorialStar.AbsoluteMagnitude"/>
+        /// </summary>
+        public double? AbsoluteMagnitude { get { return EquatorialStar.AbsoluteMagnitude; } }
+
+        /// <summary>
+        /// <see cref="EquatorialStar.Spectrum"/>
+        /// </summary>
+        public string? Spectrum { get { return EquatorialStar.Spectrum; } }
+
+        /// <summary>
+        /// <see cref="EquatorialStar.ColorIndex"/>
+        /// </summary>
+        public double? ColorIndex { get { return EquatorialStar.ColorIndex; } }
+    }
+}
