a8dcb37c4f8d100732a0c071fc1b2402bfaa8d31
djoogway <theflaffywaffe@gmail.com>
2025-09-23 00:14:47 -0500

    formatting

--- src/ChargerAstronomyShared/Domain/Horizontal/HorizontalMoon.cs
+++ src/ChargerAstronomyShared/Domain/Horizontal/HorizontalMoon.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+using ChargerAstronomyShared.Domain.Equatorial;
 
 namespace ChargerAstronomyShared.Domain.Horizontal
 {
--- src/ChargerAstronomyShared/Domain/Horizontal/HorizontalPlanet.cs
+++ src/ChargerAstronomyShared/Domain/Horizontal/HorizontalPlanet.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
+using ChargerAstronomyShared.Domain.Equatorial;
 
 namespace ChargerAstronomyShared.Domain.Horizontal
 {
