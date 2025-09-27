463f6df709e584817c9e5b6a55605e57235db601
Mason Carr <luffy42891@gmail.com>
2025-09-26 23:47:55 -0500

    some stuff

---
+++ src/ChargerAstronomyShared/Domain/Prediction/TileHeater.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargerAstronomyShared.Domain.Prediction
+{
+    using ChargerAstronomyShared.Domain.Heat;
+
+    public sealed class TileHeater
+    {
+        private readonly HeatMap heatmap;
+
+    }
+}
