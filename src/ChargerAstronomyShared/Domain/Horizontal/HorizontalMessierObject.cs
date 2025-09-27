efac7d494ad935372fb20e7f4d0a6f4ef6932a7a
djoogway <theflaffywaffe@gmail.com>
2025-09-26 20:51:08 -0500

    horizontal messier

---
+++ src/ChargerAstronomyShared/Domain/Horizontal/HorizontalMessierObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ChargerAstronomyShared.Domain.Equatorial;
+
+namespace ChargerAstronomyShared.Domain.Horizontal
+{
+    /// <summary>
+    /// A Messier Deep Space Object located in Horizontal coordinates
+    /// Author: Josh Johner
+    /// Created: SPR 2025
+    /// </summary>
+    public class HorizontalMessierObject : HorizontalBody
+    {
+        /// <summary>
+        /// Creates a new object by wrapping a <see cref="EquatorialMessierObject"/>
+        /// </summary>
+        /// <param name="body">The <see cref="EquatorialMessierObject"/> to base this object off of.</param>
+        public HorizontalMessierObject(EquatorialMessierObject body) : base(body)
+        {
+            MessierId = body.MessierId;
+            NewGeneralCatalog = body.NewGeneralCatalog;
+            Type = body.Type;
+            Constellation = body.Constellation;
+            Size = body.Size;
+            ViewingDifficulty = body.ViewingDifficulty;
+            ViewingSeason = body.ViewingSeason;
+            CommonName = body.CommonName;
+            Console.WriteLine($"Common name {CommonName}");
+        }
+
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.MessierId"/>
+        /// </summary>
+        public string? MessierId { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.NewGeneralCatalog"/>
+        /// </summary>
+        public string? NewGeneralCatalog { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.Type"/>
+        /// </summary>
+        public string? Type { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.Constellation"/>
+        /// </summary>
+        public string? Constellation { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.Size"/>
+        /// </summary>
+        public string? Size { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.ViewingSeason"/>
+        /// </summary>
+        public string? ViewingSeason { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.ViewingDifficulty"/>
+        /// </summary>
+        public string? ViewingDifficulty { get; internal set; }
+        /// <summary>
+        /// <see cref="EquatorialMessierObject.CommonName"/>
+        /// </summary>
+        public string? CommonName { get; internal set; }
+    }
+}
