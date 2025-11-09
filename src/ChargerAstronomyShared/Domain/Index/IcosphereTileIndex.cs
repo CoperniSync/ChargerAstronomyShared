5a16ca411e7e6f2dc9f1bcd12ed1f95877180e4d
Mason Carr <luffy42891@gmail.com>
2025-11-09 17:52:29 -0600

    fixed icospheretileindex missing semicolon

--- src/ChargerAstronomyShared/Domain/Index/IcosphereTileIndex.cs
+++ src/ChargerAstronomyShared/Domain/Index/IcosphereTileIndex.cs
@@ -279,7 +279,7 @@ namespace ChargerAstronomyShared.Domain.Index
                 (1,5,9),  (5,11,4), (11,10,2), (10,7,6), (7,1,8),
                 (3,9,4),  (3,4,2),  (3,2,6),  (3,6,8),  (3,8,9),
                 (4,9,5),  (2,4,11), (6,2,10), (8,6,7),  (9,8,1)
-            }
+            };
         }
 
         private readonly List<TileId> tiles = new List<TileId>();
